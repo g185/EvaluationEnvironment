907fbef32d2f012e46940155f76eab9a214028c5
g185 <giuliano.martinelli97@gmail.com>
2021-10-13 13:06:31 +0200

    c

---
+++ src/baselines/test.py
@@ -0,0 +1,5 @@
+with 
+Yake_keyword_extractor = Yake_KE()
+Rake_keyword_extractor = Rake_KE()
+print(Yake_keyword_extractor.extract_keywords("text"))
+print(Rake_keyword_extractor.extract_keywords("text"))
