a4b5ba6679268a5ef26e063389a841b9c061b7df
g185 <giuliano.martinelli97@gmail.com>
2021-10-14 11:36:42 +0200

    c

--- src/baselines/keyword_extractors.py
+++ src/baselines/keyword_extractors.py
@@ -1,25 +1,47 @@
+
 import yake
 from rake_nltk import Rake
 import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
+from nltk.stem.porter import *
+
 
 class KeywordExtractor():
     def extract_keywords(self, text: str) -> list:
+        pass
+
+    def extract_keywords_with_weights(self, text: str) -> list:
         pass
 
 
 class Yake_KE(KeywordExtractor):
     def __init__(self):
         self.kw_extractor = yake.KeywordExtractor()
+        self.stemmer = PorterStemmer()
     
     def extract_keywords(self, text: str) -> list:
-        return self.kw_extractor.extract_keywords("text")
+        weighted_keywords = self.kw_extractor.extract_keywords(text)
+        unweighted_keywords = [key[0] for key in weighted_keywords]
+        return unweighted_keywords
+    
+    def extract_stemmed_keywords(self, text: str) -> list:
+        weighted_keywords = self.kw_extractor.extract_keywords(text)
+        unweighted_keywords = [self.stemmer.stem(key[0]) for key in weighted_keywords]
+        return unweighted_keywords
+
+    def extract_keywords_with_weights(self, text: str) -> list:
+        return self.kw_extractor.extract_keywords(text)
+    
 
 class Rake_KE(KeywordExtractor):
-        def __init__(self):
-            self.kw_extractor = Rake()
+    def __init__(self):
+        self.kw_extractor = Rake()
+        self.stemmer = PorterStemmer()
 
-        def extract_keywords(self, text: str) -> list:
-            self.kw_extractor.extract_keywords_from_text(text)
-            return self.kw_extractor.get_ranked_phrases_with_scores()
+    def extract_keywords(self, text: str) -> list:
+        self.kw_extractor.extract_keywords_from_text(text)
+        return self.kw_extractor.get_ranked_phrases_with_scores()
+    
+    def extract_keywords_with_weights(self, text: str) -> list:
+        pass
--- src/baselines/test.py
+++ src/baselines/test.py
@@ -1,5 +1,30 @@
-with 
+from os import read, walk, listdir
+from keyword_extractors import Yake_KE, Rake_KE
+from sklearn.metrics import f1_score
+from nltk.stem.porter import *
+
+stemmer = PorterStemmer()
+list_of_texts = []
+filenames = listdir("../../data/SemEval2010/SemEval2010/docsutf8")
+for filename in filenames:
+    with open("../../data/SemEval2010/SemEval2010/docsutf8/" + filename) as f:
+        list_of_texts.append(f.read())
+
+list_of_keys = []
+filenames = listdir("../../data/SemEval2010/SemEval2010/keys")
+for filename in filenames:
+    with open("../../data/SemEval2010/SemEval2010/keys/" + filename) as f:
+        keys = f.read().splitlines()
+        stemmed_keys = [stemmer.stem(k) for k in keys]
+        list_of_keys.append(stemmed_keys)
+
 Yake_keyword_extractor = Yake_KE()
-Rake_keyword_extractor = Rake_KE()
-print(Yake_keyword_extractor.extract_keywords("text"))
-print(Rake_keyword_extractor.extract_keywords("text"))
+list_of_yake_answeres = [Yake_keyword_extractor.extract_stemmed_keywords(k) for k in list_of_texts] #[[(key,val)]]
+
+print(list_of_yake_answeres[0])
+print(list_of_keys[0])
+print(len(list(set(list_of_yake_answeres[0]) & set(list_of_keys[0])))/ len(list_of_keys[0]))
+
+#Rake_keyword_extractor = Rake_KE()
+#print(Yake_keyword_extractor.extract_keywords(read_data))
+#print(Rake_keyword_extractor.extract_keywords(read_data))
