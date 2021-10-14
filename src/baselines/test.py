8a9d504a6a960e63c4a086bd1e6856c7b45b6b17
g185 <giuliano.martinelli97@gmail.com>
2021-10-14 17:45:57 +0200

    c

--- src/baselines/test.py
+++ src/baselines/test.py
@@ -1,9 +1,22 @@
 from os import read, walk, listdir
 from keyword_extractors import Yake_KE, Rake_KE
-from sklearn.metrics import f1_score
 from nltk.stem.porter import *
-
 stemmer = PorterStemmer()
+
+def f1_at_k(pred, gold, k = 20):
+    if len(pred) > k:
+        pred = pred[:k]
+    tp = len(set(pred) & set(gold))
+    fp = len(set(pred) - set(gold))
+    fn = len(set(gold) - set(pred))
+    precision = tp/(tp + fp)
+    recall = tp/(tp + fn)
+    try:
+        f1_score = 2* (precision * recall)/(precision + recall)
+    except:
+        return 0.0
+    return f1_score
+"""
 list_of_texts = []
 filenames = listdir("../../data/SemEval2010/SemEval2010/docsutf8")
 for filename in filenames:
@@ -23,8 +36,13 @@ list_of_yake_answeres = [Yake_keyword_extractor.extract_stemmed_keywords(k) for 
 
 print(list_of_yake_answeres[0])
 print(list_of_keys[0])
-print(len(list(set(list_of_yake_answeres[0]) & set(list_of_keys[0])))/ len(list_of_keys[0]))
-
+mean = 0
+for i, pred in enumerate(list_of_yake_answeres):
+    f1 = f1_at_k(pred, list_of_keys[i], k = 20)
+    print(i, f1)
+    mean += f1 
+print(mean/i)
+"""
 #Rake_keyword_extractor = Rake_KE()
 #print(Yake_keyword_extractor.extract_keywords(read_data))
 #print(Rake_keyword_extractor.extract_keywords(read_data))
