43bbc6a03b8a06bd4a453ea8426e3f207ae37f29
g185 <giuliano.martinelli97@gmail.com>
2021-11-15 19:35:56 +0100

    c

--- src/test.py
+++ src/test.py
@@ -18,7 +18,7 @@ k = args.k
 list_of_texts, list_of_keys = read_texts_and_keywords(dataset_name=args.dataset_name, stem_keywords=stemming)
 print(list_of_texts[0], list_of_keys[0])
 
-Yake_keyword_extractor = BartextraggoEncoder_KE()
+Yake_keyword_extractor = BartextraggoEncoder_KE("../nextraggo/experiments/pretrain/pretrain_15-11-2021_00-00-00-v1.ckpt")
 list_of_yake_answeres = [Yake_keyword_extractor.extract_keywords(k, stemming = stemming) for k in list_of_texts] #[[(key,val)]]
 
 print(mean_f1_at_k(list_of_yake_answeres, list_of_keys, k))
